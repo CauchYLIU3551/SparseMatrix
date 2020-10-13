d9254a88bc07df330486273248ca3dff71425974
CauchYLIU3551 <72800663+CauchYLIU3551@users.noreply.github.com>
2020-10-13 20:02:45 +0800

    Add files via upload

---
+++ sparsematrix.cpp
@@ -0,0 +1,402 @@
+#include<iostream>
+#include<cstdio>
+#include<stdio.h>
+#include<vector>
+#include<math.h>
+
+//template <class T1>
+class sparsematrix
+{
+public:
+
+	sparsematrix();
+	sparsematrix(std::vector<int> n, std::vector<int> j, std::vector<double> v);
+	sparsematrix(std::vector<int> n, std::vector<int> j, std::vector<double> v, int row, int col);
+
+	sparsematrix transpose();
+	sparsematrix inverse();
+	
+	sparsematrix diag();
+	sparsematrix lowtri();
+	sparsematrix uppertri();
+
+	static sparsematrix add(sparsematrix A, sparsematrix B);
+	std::vector<double> multiply(std::vector<double> x);
+	void showmatrix();
+
+	std::vector<int> show_offset();
+	std::vector<int> show_indice();
+	std::vector<double> show_value();
+	
+	//std::vector<int> rowoffset;
+	//std::vector<int> indice;
+	//std::vector<double> value;
+
+private:
+	std::vector<int> rowoffset; // A vector that saves the data of the rowoffsets info;
+	std::vector<int> indice; // A vector that saves the col index of the nonzero element;
+	std::vector<double> value; // a vector that saves the nonzero values; 
+	int col;
+	int row;
+	int max();
+};
+//template<class T1>
+
+// default construction function without input;
+sparsematrix::sparsematrix():rowoffset({ 0 ,0 }), indice({ 0 }), value({ 0 })
+{
+	col = max();
+	row = 1;
+};
+
+// construction function with input;
+sparsematrix::sparsematrix(std::vector<int> n, std::vector<int> j, std::vector<double> v) :rowoffset(n), indice(j), value(v)
+{
+	col = max();
+	row = rowoffset.size() - 1;
+}
+
+// construction function with defined row and col;
+sparsematrix::sparsematrix(std::vector<int> n, std::vector<int> j, std::vector<double> v, int ind_row, int ind_col) :rowoffset(n), indice(j), value(v)
+{
+	row = ind_row;
+	col = ind_col;
+}
+
+// get the transpose matrix of the sparse matrix.
+sparsematrix sparsematrix::transpose()
+{
+
+	std::vector<double>v;
+	std::vector<int>n, c;
+	n.push_back(0);
+	int index = 0;
+
+	int num = 0; // compute the number of the values sit in the jth col, inorder to compute the transpose rowoffsets;
+
+	for (int j = 0; j < col; j++)
+	{
+		
+		for (int i = 0; i < indice.size(); i++)
+		{
+			if (indice[i] == j) //if the indice start from 1, here will change to indice[k]-1
+			{
+				v.push_back(value[i]);
+				num += 1;
+				for (int k = 0; k < col; k++)
+				{
+					if (rowoffset[k] <= i && i < rowoffset[k + 1])
+					{
+						c.push_back(k);
+						break;
+					}
+				}
+			}
+
+			// find the related index of the value in row and input it into the transpose col vector;
+
+		}
+		n.push_back(num);
+	}
+
+	sparsematrix A(n,c,v);
+
+	return A;
+}
+
+
+// diag() function is used to get the diagnal element of the matrix and output a Diagnal matrix D
+// we can divide the matrix in this way: A=D-L-U; D is diagnal matrix, L is lower-triangular matrix, U is upper-triangular matrix;
+sparsematrix sparsematrix::diag()
+{
+	std::vector<int> r(1,0),Dindice;
+	std::vector<double> Dvalue;
+	int flag = 0;
+	std::cout << col << std::endl;
+	for (int i = 0; i < row; i++)
+	{
+		//std::cout << "this is the " << i << "th times test!!!\n";
+		for (int j = rowoffset[i]; j < rowoffset[i + 1]; j++)
+		{
+			//std::cout << "this is the" << indice[j] << "col element!\n";
+			if (i == indice[j]) //if the indice start from 1, here will change to indice[k]-1
+			{
+				Dindice.push_back(indice[j]);
+				Dvalue.push_back(value[j]);
+				r.push_back(i+1);
+				flag = 1;
+				break;
+			}
+
+		}
+		if (flag != 1)
+		{
+			printf_s("In the %dth row the element in the diag is zero\n", i + 1);
+			r.push_back(r[i]);
+		}
+
+	}
+
+	sparsematrix D(r,Dindice,Dvalue);
+	
+	return D;
+}
+
+
+// Need to finish!!!!!!
+// Compute the lower-triangular L;
+sparsematrix sparsematrix::lowtri()
+{
+	std::vector<int> r(1, 0), Lindice;
+	std::vector<double> Lvalue;
+	for (int i = 0; i < row; i++)
+	{
+		int num = 0;
+		for (int j = rowoffset[i]; j < rowoffset[i + 1]; j++)
+		{
+			if (indice[j] < i)
+			{
+				num++;
+				Lindice.push_back(indice[j]);
+				Lvalue.push_back(value[j]);
+			}
+			else
+				break;
+		}
+		r.push_back(r[i] + num);
+	}
+	sparsematrix L(r, Lindice, Lvalue, row, col); // here I use the index of row and col index to define the sparse matrix otherwise it will lose the col info.
+	return L;
+}
+
+// Compute the upper-triangular U;
+sparsematrix sparsematrix::uppertri()
+{
+	std::vector<int> r(1, 0), Uindice;
+	std::vector<double> Uvalue;
+	for (int i = 0; i < row; i++)
+	{
+		int num = 0;
+		for (int j = rowoffset[i]; j < rowoffset[i + 1]; j++)
+		{
+			if (indice[j] > i)
+			{
+				num++;
+				Uindice.push_back(indice[j]);
+				Uvalue.push_back(value[j]);
+			}
+		}
+		r.push_back(r[i] + num);
+	}
+	sparsematrix U(r, Uindice, Uvalue, row, col); // here I use the index of row and col index to define the sparse matrix otherwise it will lose the col info.
+	return U;
+}
+
+////////////////////////need to finish !
+sparsematrix sparsematrix::inverse()
+{
+	sparsematrix A;
+	return A;
+}
+
+sparsematrix sparsematrix::add(sparsematrix A, sparsematrix B)
+{
+	std::vector<double> Cvalue;
+	std::vector<int>Cind, Crow;
+	Crow.push_back(0);
+	
+	if (A.row == B.row && A.col == B.col)
+	{
+		for (int i = 0; i < A.row; i++)
+		{
+			//std::cout << "this is No." << i << " test of this !!!!\n";
+			int len, len1, len2;
+			len1 = A.rowoffset[i + 1] - A.rowoffset[i];
+			len2 = B.rowoffset[i + 1] - B.rowoffset[i];
+
+			len = len1 + len2;
+			//std::cout << len << std::endl;
+			//std::cout << "this is No." << i << " test len1="<<len1<<"!!!!\n";
+			//std::cout << "this is No." << i << " test len2="<<len2<<"!!!!\n";
+			//std::cout << A.rowoffset[i] << std::endl;
+
+
+			int j = 0, k = 0;
+
+			while (j<len1&&k<len2)//there is a problem!
+			{
+				if (A.indice[A.rowoffset[i]+j] < B.indice[B.rowoffset[i]+k])
+				{
+					Cind.push_back(A.indice[A.rowoffset[i] + j]);
+					Cvalue.push_back(A.value[A.rowoffset[i] + j]);
+					j++;
+				}
+				else if (B.indice[B.rowoffset[i] + k] < A.indice[A.rowoffset[i] + j])
+				{
+					Cind.push_back(B.indice[B.rowoffset[i] + k]);
+					Cvalue.push_back(B.value[B.rowoffset[i] + k]);
+					k++;
+				}
+				else
+				{
+					Cind.push_back(A.indice[A.rowoffset[i] + j]);
+					Cvalue.push_back(A.value[A.rowoffset[i] + j] + B.value[B.rowoffset[i] + k]);
+					len = len - 1;
+					//std::cout << "ATTENTION!\n";
+					//std::cout << A.indice[A.rowoffset[i] + j] << std::endl;
+					//std::cout << B.indice[B.rowoffset[i] + k] << std::endl;
+					j++;
+					k++;
+				}
+			}
+			while (j < len1)
+			{
+				Cind.push_back(A.indice[A.rowoffset[i] + j]);
+				Cvalue.push_back(A.value[A.rowoffset[i] + j]);
+				j++;
+			}
+			while (k < len2)
+			{
+				Cind.push_back(B.indice[B.rowoffset[i] + k]);
+				Cvalue.push_back(B.value[B.rowoffset[i] + k]);
+				k++;
+			}
+			std::cout <<"After that"<< len << std::endl;
+			Crow.push_back(Crow[i] + len);
+		}
+
+		/*
+		for (int i = 0; i < Crow.size(); i++)
+		{
+			std::cout << Crow[i] << " ";
+		}
+		std::cout << std::endl;
+
+		for (int i = 0; i < Cind.size(); i++)
+		{
+			std::cout << Cind[i] << " ";
+		}
+		std::cout << std::endl;
+
+		for (int i = 0; i < Cvalue.size(); i++)
+		{
+			std::cout << Cvalue[i] << " ";
+		}
+		std::cout << std::endl;
+		*/
+
+		sparsematrix C(Crow, Cind, Cvalue);
+		
+		return C;
+	}
+	else
+	{
+		std::cout << "Function add() ERROR:The two matrixes are not in same size! Please check it!\n";
+	}
+}
+ 
+
+
+// compute the value of the sparsematrix times a n-dimensional vector x.
+std::vector<double> sparsematrix::multiply(std::vector<double> x)
+{
+	std::vector<double> b(row,0);
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = rowoffset[i]; j < rowoffset[i + 1]; j++)
+		{
+			b[i] += value[j] * x[indice[j]]; //if the indice start from 1, here will change to indice[k]-1
+		}
+	}
+
+	return b;
+}
+
+// print the value of the whole matrix;
+void sparsematrix::showmatrix()
+{
+	std::cout << "this is the sparse matrix:::" << std::endl;
+	std::cout << "the col is :::" << col << std::endl;
+	std::cout << "the row is :::" << row << std::endl;
+	if (col == 1)
+		std::cout << value[0] << std::endl;
+	else
+
+		for (int j = 0; j < rowoffset.size() - 1; j++)
+		{
+			int len = rowoffset[j + 1] - rowoffset[j];
+			/*
+			for (int i = 0; i < len; i++)
+			{
+				int num = indice[i + rowoffset[j]];
+				while (num > 0)
+				{
+					num = num - 1;
+				}
+			}*/
+			int k = rowoffset[j];
+			int num = 0;
+			for (int i = 0; i < col; i++)
+			{
+				if (k < rowoffset[row])
+				{
+					if (i == indice[k] && num < len) //if the indice start from 1, here will change to indice[k]-1
+					{
+						std::cout << value[k] << " ";
+						k++;
+						num++;
+					}
+					else
+						std::cout << "0 ";
+				}
+				else
+					std::cout << "0 ";
+			}
+			std::cout << std::endl;
+		}
+
+	std::cout << std::endl;
+}
+
+// A special function, that helps the class to get the num of col;
+int sparsematrix::max()
+{
+	int num = 0;
+	for (int i = 0; i < indice.size(); i++)
+	{
+		if (indice[i] >= num)
+			num = indice[i];
+	}
+	return num+1;
+};
+
+std::vector<int> sparsematrix::show_offset()
+{
+	for (int i = 0; i < rowoffset.size(); i++)
+	{
+		std::cout << rowoffset[i] << " ";
+	}
+	std::cout << std::endl;
+	return rowoffset;
+}
+
+std::vector<int> sparsematrix::show_indice()
+{
+	for (int i = 0; i < indice.size(); i++)
+	{
+		std::cout << indice[i] << " ";
+	}
+	std::cout << std::endl;
+
+	return indice;
+}
+
+std::vector<double> sparsematrix::show_value()
+{
+	for (int i = 0; i < value.size(); i++)
+	{
+		std::cout << value[i] << " ";
+	}
+	std::cout << std::endl;
+	return value;
+}
---
+++ test.cpp
@@ -0,0 +1,119 @@
+// sparsematrix.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <sparsematrix.h>
+
+int main()
+{
+    std::vector<int> n,c;
+    std::vector<double> v, x(4, 1);
+ 
+    n = { 0,2,4,7,9 };
+    c = { 0,1,1,2,0,2,3,1,3 };
+    v = { 1,7,2,8,5,3,9,6,4 };
+    sparsematrix C(n, c, v);
+
+    n = { 0,1,3,4,6 };
+    c = { 0,1,3,2,1,3};
+    v = { 1,2,6,3,5,4};
+    sparsematrix B(n, c, v);
+
+    n = { 0,2,5,8,12,16,19 };
+    c = { 0,4,0,1,5,1,2,3,0,2,3,4,1,3,4,5,1,4,5 };
+    v = { 10,-2,3,9,3,7,8,7,3,8,7,5,8,9,9,13,4,2,-1 };
+    sparsematrix A(n, c, v);
+
+    sparsematrix L,U;
+    C.showmatrix();
+    L = C.lowtri();
+    L.showmatrix();
+
+    L.show_indice();
+    L.show_offset();
+    L.show_value();
+
+    U = C.uppertri();
+    U.showmatrix();
+
+    U.show_indice();
+    U.show_offset();
+    U.show_value();
+   
+
+    /*
+    A.showmatrix();
+
+    A.show_indice();
+    A.show_offset();
+    A.show_value();
+
+    std::cout << "this is the test of the getting diagnal function!\n";
+    A = A.diag();
+    A.showmatrix();
+    A.show_offset();
+    A.show_indice();
+    A.show_value();
+    B.showmatrix();
+    B.show_indice();
+    C.showmatrix();  
+
+
+
+    std::cout << "this is the test of the add function!\n";
+
+    B=sparsematrix::add(B, C);
+
+    B.show_indice();
+    B.show_offset();
+    B.show_value();
+    B.showmatrix();
+
+    //A.showmatrix();
+    //B.showmatrix();
+
+    
+    C.showmatrix();
+    C.show_indice();
+    C.show_offset();
+    C.show_value();
+
+    
+    std::cout << "After transpose!\n";
+    A = C.transpose();
+    A.showmatrix();
+
+    A.show_indice();
+    A.show_offset();
+    A.show_value();
+
+    std::cout << "This is the multiply of the sparse matrix!!\n";
+
+    x=A.multiply(x);
+    for (int i = 0; i < x.size(); i++)
+    {
+    std::cout << x[i] << " ";
+    } 
+    
+    std::cout << "this is the test of the getting diagnal function!\n";
+    A = C.diag();
+    A.showmatrix();
+    A.show_offset();
+    A.show_indice();
+    A.show_value();
+    */
+
+
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
