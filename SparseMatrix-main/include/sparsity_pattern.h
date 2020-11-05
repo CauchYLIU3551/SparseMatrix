c536c8647e49a8a1d8240b034800dd5a270f2fdc
CauchYLIU3551 <mc05565@umac.mo>
2020-11-05 21:48:50 +0800

    update sparsity

--- SparseMatrix-main/include/sparsity_pattern.h
+++ SparseMatrix-main/include/sparsity_pattern.h
@@ -1,9 +1,17 @@
 #ifndef __sparsity_pattern_h
 #define __sparsity_pattern_h
 
-#include<sparse/sparsematrix.h>
-#include<vector>
+//#include<sparse/sparsematrix.h>
+#include <algorithm>
+#include <vector>
+#include <iostream>
+
+class SparsityPattern;
+{
+public:
+  SparsityPattern();
+  SparsityPattern(const SparsityPattern &);
 
 
+}
 
-#endif
