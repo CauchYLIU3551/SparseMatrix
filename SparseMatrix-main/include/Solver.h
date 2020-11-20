03f752b1ace13d7ce9d55b22956aaae492f03df6
CauchYLIU3551 <mc05565@umac.mo>
2020-11-20 19:55:42 +0800

    add some comments in Solver.h

--- SparseMatrix-main/include/Solver.h
+++ SparseMatrix-main/include/Solver.h
@@ -13,6 +13,11 @@ public:
 	Solver();
 	Solver(sparsematrix A);
 	std::vector<double> solve(std::vector<doube> b, double eps);
+	//
+	// In fact, in AMGSolver, GaussSidel is used in sparsematrix, and the 
+	// object is the projection matrix output from the function projection
+	// What's more, can have a try to use the point or & to refer the
+	// components. In this way, we do not need to define the return value;
 	std::vector<double> GaussSeidel(std::vector<double> b, double eps, int num);
 	std::vector<double> GaussSeidel(std::vector<std::vector<double>> IA,std::vector<double> b, double eps=1.0e-08, int num=10)
 
